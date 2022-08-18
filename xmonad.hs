f8a2807a08fa7657cbf018055ef9b8c81f523724
Adithya Kumar <adithya@composewell.com>
2022-08-18 12:48:03 +0530

    Add helpful emacs-like key translations

--- xmonad.hs
+++ xmonad.hs
@@ -46,6 +46,46 @@ copy = translateIfNotEmacs (mod1Mask, xK_w) [(controlMask, xK_c)]
 paste :: ((KeyMask, KeySym), X ())
 paste = translateIfNotEmacs (controlMask, xK_y) [(controlMask, xK_v)]
 
+lineDown :: ((KeyMask, KeySym), X ())
+lineDown = translateIfNotEmacs (controlMask, xK_n) [(noModMask, xK_Down)]
+
+lineUp :: ((KeyMask, KeySym), X ())
+lineUp = translateIfNotEmacs (controlMask, xK_p) [(noModMask, xK_Up)]
+
+search :: ((KeyMask, KeySym), X ())
+search = translateIfNotEmacs (controlMask, xK_s) [(controlMask, xK_f)]
+
+paraDown :: ((KeyMask, KeySym), X ())
+paraDown = translateIfNotEmacs (mod1Mask, xK_n) [(noModMask, xK_Next)]
+
+paraUp :: ((KeyMask, KeySym), X ())
+paraUp = translateIfNotEmacs (mod1Mask, xK_p) [(noModMask, xK_Prior)]
+
+killLine :: ((KeyMask, KeySym), X ())
+killLine =
+    translateIfNotEmacs
+        (controlMask, xK_k)
+        [(shiftMask, xK_End), (controlMask, xK_x)]
+
+movePointBegin :: ((KeyMask, KeySym), X ())
+movePointBegin = translateIfNotEmacs (controlMask, xK_a) [(noModMask, xK_Home)]
+
+charBack :: ((KeyMask, KeySym), X ())
+charBack = translateIfNotEmacs (controlMask, xK_b) [(noModMask, xK_Left)]
+
+charForward :: ((KeyMask, KeySym), X ())
+charForward = translateIfNotEmacs (controlMask, xK_f) [(noModMask, xK_Right)]
+
+wordBack :: ((KeyMask, KeySym), X ())
+wordBack = translateIfNotEmacs (mod1Mask, xK_b) [(controlMask, xK_Left)]
+
+wordForward :: ((KeyMask, KeySym), X ())
+wordForward = translateIfNotEmacs (mod1Mask, xK_f) [(controlMask, xK_Right)]
+
+-- XXX There is something wrong with this
+esc :: ((KeyMask, KeySym), X ())
+esc = translateIfNotEmacs (controlMask, xK_g) [(noModMask, xK_Escape)]
+
 main :: IO ()
 main = xmonad conf
 
@@ -58,6 +98,18 @@ main = xmonad conf
             , ((mod1Mask .|. shiftMask, xK_o), smartPrevWindow)
             , copy
             , paste
+            , lineDown
+            , lineUp
+            , search
+            , paraDown
+            , paraUp
+            -- , esc
+            , killLine
+            , movePointBegin
+            , charBack
+            , charForward
+            , wordBack
+            , wordForward
             ]
             $ def
                   { terminal = "/usr/local/bin/st"
