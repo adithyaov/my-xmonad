5e5ea2ae356b15ed07fa3921d235f1a9f8bddf67
Adithya Kumar <adithya@composewell.com>
2023-10-08 18:14:24 +0530

    Explicitly use Mod1Mask for key customization

--- xmonad.hs
+++ xmonad.hs
@@ -22,45 +22,45 @@ main = do
     conf =
         def
             { terminal = "st"
-            , modMask = mod1Mask
+            , modMask = mod4Mask
             , keys = customKeys
             }
 
 customKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
-customKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+customKeys _ = M.fromList $
     -- launching and killing programs
-    [ ((modMask .|. shiftMask, xK_0     ), spawn "rofi -show run")
+    [ ((mod1Mask .|. shiftMask, xK_0     ), spawn "rofi -show run")
     , ((controlMask .|. shiftMask, xK_Return ), spawn "rofi -show window")
-    , ((modMask .|. shiftMask, xK_c        ), kill)
+    , ((mod1Mask .|. shiftMask, xK_c        ), kill)
 
     -- move focus up or down the window stack
-    , ((modMask,               xK_Tab   ), windows W.focusDown)
-    , ((modMask .|. shiftMask, xK_Tab   ), windows W.focusUp  )
+    , ((mod1Mask,               xK_Tab   ), windows W.focusDown)
+    , ((mod1Mask .|. shiftMask, xK_Tab   ), windows W.focusUp  )
 
     -- modifying the window order
-    , ((modMask  .|. shiftMask, xK_Return), windows W.swapMaster)
+    , ((mod1Mask  .|. shiftMask, xK_Return), windows W.swapMaster)
 
     -- resizing the master/slave ratio
-    , ((modMask,               xK_Right     ), sendMessage Shrink)
-    , ((modMask,               xK_Left      ), sendMessage Expand)
+    , ((mod1Mask,               xK_Left       ), sendMessage Shrink)
+    , ((mod1Mask,               xK_Right      ), sendMessage Expand)
 
     -- quit, or restart
-    -- , ((modMask .|. shiftMask, xK_q     ), io exitSuccess) -- %! Quit xmonad
+    -- , ((mod1Mask .|. shiftMask, xK_q     ), io exitSuccess) -- %! Quit xmonad
 
-    , ((modMask .|. shiftMask, xK_slash ), helpCommand)
+    , ((mod1Mask .|. shiftMask, xK_slash ), helpCommand)
     -- repeat the binding for non-American layout keyboards
-    , ((modMask              , xK_question), helpCommand)
+    , ((mod1Mask              , xK_question), helpCommand)
     ]
     ++
     -- mod-[1..9] %! Switch to workspace N
     -- mod-shift-[1..9] %! Move client to workspace N
-    [((m .|. modMask, k), windows $ f i)
+    [((m .|. mod1Mask, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
     ++
     -- mod-{w,e,r} %! Switch to physical/Xinerama screens 1, 2, or 3
     -- mod-shift-{w,e,r} %! Move client to screen 1, 2, or 3
-    [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+    [((m .|. mod1Mask, key), screenWorkspace sc >>= flip whenJust (windows . f))
         | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
