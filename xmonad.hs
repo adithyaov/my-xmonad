a15891e336164904bd3ed526556d59c6088678e0
Adithya Kumar <adithya@composewell.com>
2023-10-08 17:08:53 +0530

    Use a basic xmonad config

--- xmonad.hs
+++ xmonad.hs
@@ -9,111 +9,62 @@ import XMonad.Hooks.StatusBar.PP
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Util.NamedWindows
 
+import qualified Data.Map as M
 import qualified XMonad.StackSet as W
 
-import Data.List (isPrefixOf)
-
-logClassName :: Logger
-logClassName = withWindowSet $ traverse (fmap show . getNameWMClass) . W.peek
-
-smartNextWindow :: X ()
-smartNextWindow = do
-    mtitle <- logClassName
-    case mtitle of
-        Nothing -> return ()
-        Just title ->
-            if title == "emacs"
-            then sendKey mod1Mask xK_o
-            else windows W.focusDown
-
-smartPrevWindow :: X ()
-smartPrevWindow = do
-    mtitle <- logClassName
-    case mtitle of
-        Nothing -> return ()
-        Just title ->
-            if title == "emacs"
-            then sendKey (mod1Mask .|. shiftMask) xK_o
-            else windows W.focusUp
-
-translateIfNotEmacs ::
-       (KeyMask, KeySym) -> [(KeyMask, KeySym)] -> ((KeyMask, KeySym), X ())
-translateIfNotEmacs (maskF, keyF) keySeq =
-    ( (maskF, keyF)
-    , do mtitle <- logClassName
-         case mtitle of
-             Nothing -> return ()
-             Just title ->
-                 if title == "emacs"
-                 then sendKey maskF keyF
-                 else sequence_ $ uncurry sendKey <$> keySeq)
-
-copy :: ((KeyMask, KeySym), X ())
-copy = translateIfNotEmacs (mod1Mask, xK_w) [(controlMask, xK_c)]
-
-paste :: ((KeyMask, KeySym), X ())
-paste = translateIfNotEmacs (controlMask, xK_y) [(controlMask, xK_v)]
-
-lineDown :: ((KeyMask, KeySym), X ())
-lineDown = translateIfNotEmacs (controlMask, xK_n) [(noModMask, xK_Down)]
-
-lineUp :: ((KeyMask, KeySym), X ())
-lineUp = translateIfNotEmacs (controlMask, xK_p) [(noModMask, xK_Up)]
-
-search :: ((KeyMask, KeySym), X ())
-search = translateIfNotEmacs (controlMask, xK_s) [(controlMask, xK_f)]
-
-paraDown :: ((KeyMask, KeySym), X ())
-paraDown = translateIfNotEmacs (mod1Mask, xK_n) [(noModMask, xK_Next)]
-
-paraUp :: ((KeyMask, KeySym), X ())
-paraUp = translateIfNotEmacs (mod1Mask, xK_p) [(noModMask, xK_Prior)]
-
-killLine :: ((KeyMask, KeySym), X ())
-killLine =
-    translateIfNotEmacs
-        (controlMask, xK_k)
-        [(shiftMask, xK_End), (controlMask, xK_x)]
-
-movePointBegin :: ((KeyMask, KeySym), X ())
-movePointBegin = translateIfNotEmacs (controlMask, xK_a) [(noModMask, xK_Home)]
-
-charBack :: ((KeyMask, KeySym), X ())
-charBack = translateIfNotEmacs (controlMask, xK_b) [(noModMask, xK_Left)]
-
-charForward :: ((KeyMask, KeySym), X ())
-charForward = translateIfNotEmacs (controlMask, xK_f) [(noModMask, xK_Right)]
-
-wordBack :: ((KeyMask, KeySym), X ())
-wordBack = translateIfNotEmacs (mod1Mask, xK_b) [(controlMask, xK_Left)]
-
-wordForward :: ((KeyMask, KeySym), X ())
-wordForward = translateIfNotEmacs (mod1Mask, xK_f) [(controlMask, xK_Right)]
-
--- XXX There is something wrong with this
-esc :: ((KeyMask, KeySym), X ())
-esc = translateIfNotEmacs (controlMask, xK_g) [(noModMask, xK_Escape)]
-
 main :: IO ()
-main = xmonad $ ewmhFullscreen $ ewmh $ xmobarProp $ conf
+main = do
+    dirs <- getDirectories
+    flip launch dirs $ ewmhFullscreen $ ewmh $ xmobarProp $ conf
 
     where
 
-    keysNormal =
-        [ ((mod1Mask, xK_o), smartNextWindow)
-        , ((mod1Mask .|. shiftMask, xK_o), smartPrevWindow)
-        , copy
-        , paste
-        ]
-    keysWorkspaces =
-        [ ((m .|. mod1Mask, k), windows $ f i)
-        | (i, k) <- zip (workspaces conf) [xK_1 .. xK_3]
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
-        ]
     conf =
-        flip additionalKeys (keysNormal ++ keysWorkspaces)
-            $ def
-                  { terminal = "/usr/local/bin/st"
-                  , modMask = mod4Mask
-                  , handleEventHook = serverModeEventHook
-                  }
+        def
+            { terminal = "st"
+            , modMask = mod1Mask
+            , keys = customKeys
+            }
+
+customKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
+customKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+    -- launching and killing programs
+    [ ((modMask .|. shiftMask, xK_0     ), spawn "rofi -show run")
+    , ((controlMask .|. shiftMask, xK_Return ), spawn "rofi -show window")
+    , ((modMask .|. shiftMask, xK_c        ), kill)
+
+    -- move focus up or down the window stack
+    , ((modMask,               xK_Tab   ), windows W.focusDown)
+    , ((modMask .|. shiftMask, xK_Tab   ), windows W.focusUp  )
+
+    -- modifying the window order
+    , ((modMask  .|. shiftMask, xK_Return), windows W.swapMaster)
+
+    -- resizing the master/slave ratio
+    , ((modMask,               xK_Right     ), sendMessage Shrink)
+    , ((modMask,               xK_Left      ), sendMessage Expand)
+
+    -- quit, or restart
+    -- , ((modMask .|. shiftMask, xK_q     ), io exitSuccess) -- %! Quit xmonad
+
+    , ((modMask .|. shiftMask, xK_slash ), helpCommand)
+    -- repeat the binding for non-American layout keyboards
+    , ((modMask              , xK_question), helpCommand)
+    ]
+    ++
+    -- mod-[1..9] %! Switch to workspace N
+    -- mod-shift-[1..9] %! Move client to workspace N
+    [((m .|. modMask, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+    ++
+    -- mod-{w,e,r} %! Switch to physical/Xinerama screens 1, 2, or 3
+    -- mod-shift-{w,e,r} %! Move client to screen 1, 2, or 3
+    [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+
+    where
+
+    helpCommand :: X ()
+    helpCommand = xmessage "See the xmonad.hs for all the bindings"
