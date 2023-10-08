4c7957240ca8e1f042feedf7d754fe135f6cdcc1
Adithya Kumar <adithya@composewell.com>
2023-10-08 16:39:25 +0530

    Add a default.nix file

---
+++ default.nix
@@ -0,0 +1,46 @@
+# CAUTION! a spelling mistake in arg string is ignored silently.
+#
+# To use ghc-8.10.7
+# nix-shell --argstr compiler "ghc8107"
+
+{
+  nixpkgs ?
+    import (builtins.fetchTarball
+      https://github.com/NixOS/nixpkgs/archive/refs/tags/23.05.tar.gz)
+        {}
+, compiler ? "default"
+}:
+let
+    utils =
+      let src = fetchGit {
+            url = "git@github.com:composewell/composewell-haskell.git";
+            ref = "master";
+          }; in (import "${src}/utils.nix") { inherit nixpkgs; };
+
+
+    haskellPackages =
+      let src = fetchGit {
+            url = "git@github.com:composewell/composewell-haskell.git";
+            ref = "master";
+          }; in (import "${src}/haskellPackages.nix")
+            { inherit nixpkgs;
+              inherit compiler; };
+
+    mkHaskellPackages = inShell:
+      haskellPackages.override (old: {
+        overrides =
+          nixpkgs.lib.composeExtensions
+            (old.overrides or (_: _: {}))
+            (with nixpkgs.haskell.lib; self: super: {
+                my-xmonad =
+                  utils.local super "my-xmonad" ./. "" inShell;
+            });
+      });
+
+    shellDrv = mkHaskellPackages true;
+
+    shell = utils.mkShell shellDrv (p: [p.my-xmonad]) true;
+
+in if nixpkgs.lib.inNixShell
+   then shell
+   else (mkHaskellPackages false).my-xmonad
